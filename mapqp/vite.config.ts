fe29c0cbc2db1325316499918ea2b77b4b7c0dc5
CommandTech <lpbachman@wpi.edu>
2026-09-02 09:29:07 -0400

    Supposedly react compiler is added

--- mapqp/vite.config.ts
+++ mapqp/vite.config.ts
@@ -1,9 +1,14 @@
 import { reactRouter } from "@react-router/dev/vite";
+import babel from '@rolldown/plugin-babel';
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [
+    tailwindcss(), 
+    reactRouter(),    
+    babel({}),
+  ],
   resolve: {
     tsconfigPaths: true,
   },
